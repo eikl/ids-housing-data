fc09d1b737f26f5ba86e9d9b0d4caedfb348d29c
h0gm4n <atte.hogman@gmail.com>
2025-09-25 18:49:27 +0300

    modify readme

--- housingdata/src/App.jsx
+++ housingdata/src/App.jsx
@@ -19,15 +19,18 @@ const App = () => {
     setApartmentPrice(event.target.value)
   }
 
+  /*
+  Get values from forms and send to backend
+  */
   const getApartments = (event) => {
     event.preventDefault()
     fetch('http://127.0.0.1:5000/', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        postalCode: '12345',
-        livingSpace: 50,
-        apartmentPrice: 100000
+        postalCode: postalCode,
+        livingSpace: livingSpace,
+        apartmentPrice: apartmentPrice
     })
     });
   }
